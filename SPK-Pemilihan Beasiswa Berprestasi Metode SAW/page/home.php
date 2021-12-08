1fe70ad455d8a37e0cee552a3bb8f9dd994e746d
HanyFitriaruli017 <91929820+HanyFitriaruli017@users.noreply.github.com>
2021-12-08 22:10:55 +0700

    Add files via upload

---
+++ SPK-Pemilihan Beasiswa Berprestasi Metode SAW/page/home.php
@@ -0,0 +1,18 @@
+<body bgcolor="bluesky">
+<h1><p align="center">
+<font color="black">
+Selamat Datang di Aplikasi  <br>
+Pemilihan Beasiswa Prestasi di IAIN Kediri <br>
+</h1> 
+</b>
+</p>
+</font>
+<html>
+<head>
+ <style>
+   body {
+    background-image: url("gambar/gambar2.jpg");
+   }
+        
+    </style>
+</head>
